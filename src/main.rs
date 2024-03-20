a09bcaca8d58f30f00beb97d169e1d0dc03c0e06
Carl Schader <carlschader@gmail.com>
2024-03-20 08:49:09 -0600

    added support for dataset directories. There is a lot of redundant code here making sure there is no file name collisions which shouldn't even be possible (hence the seen map)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::fs;
+use std::{fs, collections::HashMap};
 use rand::prelude::*;
 
 /// Rust program that splits a directory full of files into train test and validation subdirs
@@ -22,12 +22,48 @@ struct Cli {
     /// Validation dir size, if not provided val dir will not be created
     #[arg(short, long)]
     val: Option<u32>,
+
+    /// DatasetDirectory: If the directory is a pytorch dataset directory
+    #[arg(short, long, default_value = "false")]
+    dataset_directory: bool,
 }
+
 
 fn main() {
     let cli = Cli::parse();
     let in_place: bool;
-    let entries = fs::read_dir(&cli.input_dir).unwrap();
+    let mut seen: HashMap<&String, u32> = HashMap::new();
+    let mut files: Vec<String> = Vec::new();
+    
+    if cli.dataset_directory {
+        for dir_entry in fs::read_dir(&cli.input_dir).unwrap() {
+            let dir_path = dir_entry.unwrap().path();
+            if dir_path.is_file() {
+                eprintln!("Error: Root directory should not contain files in DATASET_DIRECTORY mode");
+            }
+
+            for entry in fs::read_dir(dir_path).unwrap() {
+                let file_path = entry.unwrap().path();
+                if file_path.is_file() {
+                    files.push(file_path.to_str().unwrap().to_string());
+                } else {
+                    eprintln!("Error: Subdirectories should only contain files in DATASET_DIRECTORY mode");
+                }
+            }
+        }
+    } else {
+        for entry in fs::read_dir(&cli.input_dir).unwrap() {
+            let path = entry.unwrap().path();
+            if path.is_file() {
+                files.push(path.to_str().unwrap().to_string());
+            } else {
+                eprintln!("Error: Root should only contain files not directories");
+            }
+        }
+    };
+
+    // shuffle the files
+    files.shuffle(&mut thread_rng());
 
     let root: String = match cli.output_dir {
         Some(output_dir) => {
@@ -42,15 +78,6 @@ fn main() {
         },
     };
     
-    let mut files: Vec<String> = Vec::new();
-    for entry in entries {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        if path.is_file() {
-            files.push(path.to_str().unwrap().to_string());
-        }
-    }
-
     let train_dir:String = format!("{}/train", root);
     let test_dir: String = format!("{}/test", root);
     fs::create_dir_all(&train_dir).unwrap();
@@ -79,38 +106,61 @@ fn main() {
     files.shuffle(&mut rng);
 
     let test_files = &files[0..test_size as usize];
-
-    for file in test_files {
-        let file_name = file.split("/").last().unwrap();
-        let dest = format!("{}/{}", test_dir, file_name);
-        if in_place {
-            fs::rename(file, dest).unwrap();
-        } else {
-            fs::copy(file, dest).unwrap();
-        }
-    }
+    create_files(test_files, &test_dir, &mut seen, in_place, cli.dataset_directory);
 
     if val_dir.is_some() {
         let val_files = &files[test_size as usize..(test_size + val_size) as usize];
-        for file in val_files {
-            let file_name = file.split("/").last().unwrap();
-            let dest = format!("{}/{}", val_dir.as_ref().unwrap(), file_name);
-            if in_place {
-                fs::rename(file, dest).unwrap();
-            } else {
-                fs::copy(file, dest).unwrap();
-            }
-        }
+        create_files(val_files, &val_dir.unwrap(), &mut seen, in_place, cli.dataset_directory);
     }
 
     let train_files = &files[(test_size + val_size) as usize..];
-    for file in train_files {
+    create_files(train_files, &train_dir, &mut seen, in_place, cli.dataset_directory);
+
+    if cli.dataset_directory {
+        for entry in fs::read_dir(&root).unwrap() {
+            let dir_path = entry.unwrap().path();
+            if dir_path.file_name().unwrap() != "train" && dir_path.file_name().unwrap() != "test" && dir_path.file_name().unwrap() != "val" {
+                fs::remove_dir_all(dir_path).unwrap();
+            }
+        }
+    }
+}
+
+fn create_files<'a>(files: &'a [String], out_dir: &str, seen: &mut HashMap<&'a String, u32>, rename: bool, dataset_dir: bool) {
+    for file in files {
         let file_name = file.split("/").last().unwrap();
-        let dest = format!("{}/{}", train_dir, file_name);
-        if in_place {
+        let mut dest: String;
+
+        if dataset_dir {
+            let class = file.split("/").nth(1).unwrap();
+            let class_dir = format!("{}/{}", out_dir, class);
+            fs::create_dir_all(&class_dir).unwrap();
+            dest = format!("{}/{}", class_dir, file_name);
+        } else {
+            dest = format!("{}/{}", out_dir, file_name);
+        }
+
+        if seen.contains_key(&file) {
+            let count = seen.get(&file).unwrap();
+            let has_extension = file_name.contains(".");
+            if has_extension {
+                let splits = file_name.split(".").collect::<Vec<&str>>();
+                let extension = splits.last().unwrap();
+                let rest = splits[0..splits.len() - 1].join(".");
+                dest = format!("{}/{}-{}.{}", out_dir, rest, count, extension);
+            } else {
+                dest = format!("{}/{}-{}", out_dir, file_name, count);
+            }
+            seen.insert(&file, count + 1);
+        } else {
+            seen.insert(&file, 1);
+        }
+
+        if rename {
             fs::rename(file, dest).unwrap();
         } else {
             fs::copy(file, dest).unwrap();
         }
     }
 }
+
